ee2441eaad9a459eb0d8ee1d702621ab5e28ce5e
Brooke Zwayer <brookezwayer@Olivias-MacBook-Pro.local>
2019-11-01 17:13:08 -0500

    laying out data structure

---
+++ js/main.js
@@ -0,0 +1,14 @@
+/* Data Structure
+
+    master array of all drinks (objects) and key value pairs
+
+    array of "easy drinks"
+
+    array of "medium" drinks
+
+    array of "hard" drinks
+
+Pull objects via getElementById for all ingredient buttons
+
+
+*/
