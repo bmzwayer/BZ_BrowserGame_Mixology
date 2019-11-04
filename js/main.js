c6343ebabdc0a089b423fb517f91f620e91a5231
Brooke Zwayer <brookezwayer@Olivias-MacBook-Pro.local>
2019-11-04 11:56:04 -0600

    added well images and grabbed by id in js, working to build two drinks to startt

--- js/main.js
+++ js/main.js
@@ -29,13 +29,10 @@ Pull objects via getElementById for all ingredient buttons
 17. Sex on the Beach: .5oz Peach Schnapps, 1.5oz OJ or pineapple juice, 1.5oz Cranberry juice, 
 
 
-
-
-
-
 //one issue is going to be what if theres a range of how many oz could be in that cocktail depending on who makes it
 
 */
+
 
 //need a startgame function that takes you to level 1 page
 //timer set/ initiate function
@@ -45,28 +42,25 @@ Pull objects via getElementById for all ingredient buttons
 //event listener for all objects on page
 //object chose must reflect object color in the cup
 
-const drinks = [ 
-    {drinkName: Old Fashioned, 
-        2oz bourbon, 2 dashes bitters, 1tsp sugar, Garnish: orange twist},
-    {Margarita: 2oz tequilla, 1oz Cointreau, 1oz Lime Juice, Garnish: Salt on the rim},
-    {Cosmopolitan: 1.5oz Citrus Vodka, 1oz Cointreau, .5oz Lime Juice, .25 oz cranberry juice},
-    {Negroni: 1oz Gin, 1ox Campari, 1oz Vermouth},
-    {Moscow Mule: 2oz Vodka, 5oz Ginger Beer, .5oz Lime Juice},
-    {Mojito: 2oz White Rum, .75oz Lime Juice, .5oz Simple Syrup, Mint leaves},
-    {Whiskey Sour: 2oz Whiskey, 1oz Lemon Juice, 1tsp sugar, 1 eggwhite},
-    {Manhattan: 2oz Rye Whiskey, 1oz Vermouth, 2 dashes bitters},
-    {Gimlet: 2oz Gin or Vodka, .75oz Simply Syrup, .75oz Lime Juice},
-    {Sazerac: 2oz Rye Whiskey, .5oz Simple Syrup, 2 dashes bitters, Absinthe},
-    {Paloma: 2oz Tequilla, .5oz Lime Juice, Grapefruit soda to top},
-    {Sidecar: 2oz VSOP Cognac, 1oz Cointreau, .75oz Lemon Juice},
-    {Mint Julep: 2oz Bourbon, Mint leaves, .25oz Simple Syrup},
-    {French 75: 2oz Gin, 2 dashes Simple Syrup, .5oz Lemon Juice, Champagne},
-    {Long Island: .5oz Vodka, .5oz Gin, .5oz Light Rum, .5oz Tequilla, .5oz Triple Sec, 2oz Sour, top with cola},
-    {White Russian: 1.5oz Vodka, 1oz Kahlua, top with milk},
-    {Sex on the Beach: .5oz Peach Schnapps, 1.5oz OJ or pineapple juice, 1.5oz Cranberry juice}
-]
+const drinks = [oldFashioned, margarita];
+    
+
 const easyDrinks = []
 
 const mediumDrinks = []
 
 const difficultDrinks = []
+
+const bourbonEl= document.getElementById(bourbon);
+const vodkaEl= document.getElementById(vodka);
+const ginEl= document.getElementById(gin);
+const whiskeyEl= document.getElementById(whiskey);
+const tequillaEl= document.getElementById(tequilla);
+const rumEl= document.getElementById(rum);
+const lightRumEl= document.getElementById(lightRum);
+
+const bittersEl= document.getElementById(bitters);
+
+const sugarEl= document.getElementById(sugar);
+const orangePeelEl= document.getElementById(orangePeel);
+
